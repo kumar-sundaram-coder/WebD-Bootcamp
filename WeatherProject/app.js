9bc519c3a565bf87804169077ea920df6773f900
Kumar Sundaram <sandyroshan2000@gmail.com>
2021-04-27 01:12:33 +0530

    .

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,38 +1,45 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  const query = req.body.cityName;
+  const apikey = "3c9fd18cd2553f7831d9ac4c58187f96";
+  const units = "metric";
   const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=3c9fd18cd2553f7831d9ac4c58187f96&units=metric";
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    query +
+    "&appid=" +
+    apikey +
+    "&units=" +
+    units;
 
   https.get(url, (response) => {
-    // console.log(response);
-
     console.log(response.statusCode);
 
     response.on("data", (data) => {
       const weatherData = JSON.parse(data);
       const temp = weatherData.main.temp;
       const weatherDescription = weatherData.weather[0].description;
-
       const icon = weatherData.weather[0].icon;
+
       const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
-      res.write("<p>The weather is currently" + weatherDescription + "</p>");
-      res.write("<h1>The Temperature in Delhi is" + temp + " Deg Celcius</h1>");
+      res.write("<p>The weather is currently " + weatherDescription + "</p>");
+      res.write(
+        "<h1>The Temperature in " + query + " is " + temp + " Deg Celcius</h1>"
+      );
       res.write("<img src=" + imageURL + ">");
       res.send();
-
-      //   console.log(temp);
-      //   console.log(weatherDescription);
-
-      //   console.log(weatherData);
-      //   console.log(data);
     });
   });
-
-  //   res.send("Server is Up and running...");
 });
 
 app.listen(3000, () => {
