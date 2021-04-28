413bf250087b52489be89bc50dc5f7fcbfced9f4
Kumar Sundaram <sandyroshan2000@gmail.com>
2021-04-28 18:10:02 +0530

    .

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,48 +1,49 @@
-let express = require("express");
-let bodyParser = require("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
-let app = express();
+const app = express();
+const items = ["Buy Food", "Coo  Food", "Eat Food"];
+const workItems = [];
+
+//ejs
 app.set("view engine", "ejs");
 
 //using bodyparser
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  let today = new Date();
-  let currentDay = today.getDay();
-  let day = "";
+  const day = date.getDate();
+  // const day = date.getDay();
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-
-    default:
-      console.log("Error: Current Day is equal to: " + currentDay);
-      break;
-  }
-  res.render("list", { kindOfDay: day });
+  res.render("list", { listTitle: day, newListItems: items });
 });
 
-// listen to the foloowing port i.e., 3000
+app.post("/", (req, res) => {
+  const item = req.body.newItem;
+  console.log(req.body);
+
+  if (req.body.list === "Work List") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", (req, res) => {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.post("/work", (req, res) => {
+  const item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+
+// listen to the following port i.e., 3000
 app.listen(3000, () => {
   console.log("Server is running on Port 3000");
 });
