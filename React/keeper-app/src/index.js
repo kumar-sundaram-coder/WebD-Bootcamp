746e07386321d93733427deedd5fa8069d42f6db
Kumar Sundaram <sandyroshan2000@gmail.com>
2021-05-09 18:11:30 +0530

    Part 1

--- React/keeper-app/src/index.js
+++ React/keeper-app/src/index.js
@@ -1,7 +1,4 @@
 //1. Create a new React app.
-import React from "react";
-import ReactDOM from "react-dom";
-
 //2. Create a App.jsx component.
 //3. Create a Header.jsx component that renders a <header> element
 //to show the Keeper App name in an <h1>.
@@ -12,4 +9,10 @@ import ReactDOM from "react-dom";
 //6. Make sure that the final website is styled like the example shown here:
 //https://l1pp6.csb.app/
 
-//HINT: You will need to study the classes in teh styles.css file to appy styling.
+//HINT: You will need to study the classes in the styles.css file to apply styling.
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
