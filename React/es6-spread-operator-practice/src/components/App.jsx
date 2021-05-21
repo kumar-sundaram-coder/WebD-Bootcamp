a9940f2a173823ebb11e726e72dd0205450f336f
Kumar Sundaram <sandyroshan2000@gmail.com>
2021-05-21 11:35:31 +0530

    ....

--- React/es6-spread-operator-practice/src/components/App.jsx
+++ React/es6-spread-operator-practice/src/components/App.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  const changeHandler = (event) => {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  };
+
+  const addItem = () => {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
+    setInputText("");
+  };
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input type="text" value={inputText} onChange={changeHandler} />
+        <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {items.map((todoItem) => {
+            return <li>{todoItem}</li>;
+          })}
         </ul>
       </div>
     </div>
